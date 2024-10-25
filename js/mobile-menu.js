26964aaa3ebac0bf818d69d5a574f4750fa5589c
Mari <mrs.tereshchenko@gmail.com>
2024-10-25 14:29:00 +0200

    final mob

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,10 +3,32 @@
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
+    links: document.querySelectorAll('[data-menu] .mobile-menu__link'), 
   };
+
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
+  document.addEventListener('click', onDocumentClick);
+
+  
+  refs.links.forEach(link => {
+    link.addEventListener('click', () => {
+      toggleModal(); 
+    });
+  });
+
   function toggleModal() {
-    refs.menu.classList.toggle('is-open');
+    const isOpen = refs.menu.classList.toggle('is-open');
+    refs.openMenuBtn.setAttribute('aria-expanded', isOpen);
+  }
+
+  function onDocumentClick(event) {
+    const isMenuOpen = refs.menu.classList.contains('is-open');
+    const isClickInsideMenu = refs.menu.contains(event.target);
+    const isClickOnOpenBtn = refs.openMenuBtn.contains(event.target);
+
+    if (isMenuOpen && !isClickInsideMenu && !isClickOnOpenBtn) {
+      toggleModal();
+    }
   }
 })();
